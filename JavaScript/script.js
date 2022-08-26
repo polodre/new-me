076c62311b89611ff9b676ba046369fc53de8f66
Andre Lucas <andre.lucas284@gmail.com>
2022-08-26 02:15:40 -0400

    starting javascript

---
+++ JavaScript/script.js
@@ -0,0 +1,18 @@
+console.log("make this show in the console");
+
+var studentName = "Andre";
+var studentAge = 38;
+var message = "My name is ";
+
+console.log(`My name is  ${studentName}`);
+console.log(`${message} ${studentName}`);
+
+var name = "Andre";
+var pets = 3;
+var funFact = "is rich";
+
+name = "Adrian";
+pets = 2;
+funFact = "is rich";
+
+console.log(`My brother name is ${name}. I have ${pets} pets. My brother Andre ${funFact} `);
